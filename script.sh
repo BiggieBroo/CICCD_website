1fe89f786d266827ab6c40f291966da4a734aa75
biggiebroo <sarvar.kulmatov.bigbro@gmail.com>
2024-03-30 14:24:17 +0100

    Updated

--- script.sh
+++ script.sh
@@ -2,13 +2,13 @@
 
 # Taking a title of the image
 export IMAGE_NAME=$1
-# export USERNAME=$2
-# export PASSWORD=$3
+export USERNAME=$2
+export PASSWORD=$3
 
 # Installing necessary parts
-# sudo yum update -y && yum install docker -y
-# sudo systemctl start docker
+sudo yum update -y && yum install docker -y
+sudo systemctl start docker
 curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-# echo $PASSWORD | docker login -u $USERNAME --password-stdin
-# docker-compose -f $IMAGE_NAME up --detach
+echo $PASSWORD | docker login -u $USERNAME --password-stdin
+docker-compose -f $IMAGE_NAME up --detach
